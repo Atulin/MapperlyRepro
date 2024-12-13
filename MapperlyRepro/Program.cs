df731dbea781d334228a712b069ad606bdb06e94
Atulin <11233299+Atulin@users.noreply.github.com>
2024-12-13 23:59:54 +0100

    Fixed files

--- MapperlyRepro/Program.cs
+++ MapperlyRepro/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Riok.Mapperly.Abstractions;
+
+namespace MapperlyRepro;
+
+public sealed class Source
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public DateTime? NotInTarget { get; set; }
+}
+
+public sealed record Target(int Id, string Name, DateTime? NotInSource);
+
+[Mapper]
+public static partial class Mapper
+{
+	public static partial IQueryable<Target> Map(IQueryable<Source> sources);
+}
